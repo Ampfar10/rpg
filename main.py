5e7d14e833b75ef3ec5ba35d4eddc15d6a3edbcc
Ampfar10 <94465412+Ampfar10@users.noreply.github.com>
2022-12-30 17:44:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,9 +4,13 @@ import random
 players = ["player1", "player2"]
 health = [100, 100]
 points = [0, 0]
+armor = [None, None]
 
 # Create a list of weapon names and damages
 weapons = [("pistol", 10, 20), ("rifle", 15, 25), ("sniper", 20, 30)]
+
+# Create a dictionary of armor names and damage reduction values
+armors = {"light armor": 5, "medium armor": 10, "heavy armor": 15}
 
 # Main game loop
 while True:
@@ -14,9 +18,22 @@ while True:
     attacker = random.choice(players)
     index = players.index(attacker)
 
-    # Choose a random player to be attack
+    # Choose a random player to be attacked
     defender = players[index - 1]
     defender_index = players[index - 1]
+
+    # Print the store menu
+    print("--- Store ---")
+    for i, (name, reduction) in enumerate(armors.items()):
+        print(f"{i + 1}: {name} ({reduction} damage reduction)")
+    print("Enter 0 to skip the store")
+
+    # Ask the attacker to choose an armor or skip the store
+    choice = int(input())
+    if choice > 0:
+        armor_name, reduction = list(armors.items())[choice - 1]
+        armor[index] = armor_name
+        print(f"{attacker} buys {armor_name}")
 
     # Ask the attacker to choose a weapon
     print(f"{attacker}, choose a weapon:")
@@ -27,6 +44,11 @@ while True:
 
     # Calculate the damage
     damage = random.randint(min_damage, max_damage)
+    
+    # Apply the damage reduction from the armor, if any
+    if armor[defender_index]:
+        damage -= armors[armor[defender_index]]
+        damage = max(0, damage)
 
     # Subtract the damage from the defender's health
     health[defender_index] -= damage
